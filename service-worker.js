89feab3de9881bf732bee28758bf81c7513dd5f2
Mickgig <michael.giguere@arbraska.com>
2025-11-18 13:19:47 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,12 @@
-const CACHE_NAME = "sanitary-inspection-arbraska-logo-v1";
+const CACHE_NAME = "sanitary-arbraska-logo-v1";
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.json",
-  "./upla-logo.png",
-  "./arbraska-logo.png",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./","./index.html","./styles.css","./app.js","./manifest.json",
+  "./arbraska-logo.png","./icon-192.png","./icon-512.png"
 ];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k))))
+));
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
